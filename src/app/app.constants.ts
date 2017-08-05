94cfa2232a705f38e98182ac4be47b0a20c4267b
Amr Abdelraouf <ga47wuj@mytum.de>
2017-08-05 22:10:42 +0200

    add app constants

---
+++ src/app/app.constants.ts
@@ -0,0 +1,29 @@
+export const WOUND_TYPES: [string] = [
+  "Venöses Ulcus",
+  "Arterielles Ulcus",
+  "Venöses u. arterielles (gemischtes) Ulcus",
+  "Diabetische Fußläsion",
+  "Vaskulitis",
+  "Verbrennung",
+  "Schrufwunde",
+  "Postoperative Wundheilungsstörung",
+  "Postoperative Wundversorgung",
+  "Dekubitus (Grad )",
+  "Erstwunde",
+  "Rezidiv"
+];
+
+export const WOUND_REASONS: [string] = [
+  "Trauma",
+  "Diabetes mellitus",
+  "Arterielle Verschlusskrankheit",
+  "Chronisch venöse Insuffizienz",
+  "Polyneuropathie",
+  "Immobilität",
+  "Infektion systemisch/lokal",
+  "Maligner Tumor",
+  "Immunosuppression",
+  "Adipositas",
+  "Mangelernährung (BMI= )",
+  "Kachexie"
+];
