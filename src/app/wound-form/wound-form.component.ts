a3326f70ffebd2b0728eb7f14105e50c42a8b959
Amr Abdelraouf <ga47wuj@mytum.de>
2017-08-05 22:39:46 +0200

    add wound form page

--- src/app/wound-form/wound-form.component.ts
+++ src/app/wound-form/wound-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from "@angular/router";
+
+import { Wound } from '@models/Wound';
+import { WOUND_TYPES, WOUND_REASONS} from '@app/app.constants';
+import { WoundService } from '@app/_services/wound.service'
 
 @Component({
   selector: 'app-wound-form',
@@ -6,10 +12,37 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class WoundFormComponent implements OnInit {
+  private woundForm: FormGroup;
+  private WOUND_TYPES: [string];
+  private WOUND_REASONS: [string];
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+            private woundService: WoundService,
+            private router: Router,
+            private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.WOUND_TYPES = WOUND_TYPES;
+    this.WOUND_REASONS = WOUND_REASONS;
+    this.woundForm = this.formBuilder.group({
+      type: ['', Validators.required],
+      reason: ['', Validators.required],
+      position: [null, Validators.required],
+    })
+  }
+
+  onSubmit() {
+    this.route.params.subscribe( params => {
+      var patientId = params['patientId'];
+      var newWound = <Wound>this.woundForm.value;
+      console.log(patientId, newWound);
+      this.woundService.createWound(patientId, newWound).subscribe( (wound: Wound) => {
+        console.log(wound);
+      });
+    })
+    /*this.patientService.createPatient(newPatient).subscribe( (patient: Patient) => {
+      this.router.navigate(['/patient', patient._id]);
+    });*/
   }
 
 }
