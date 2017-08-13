a4d2a85d096d02e4f46e505ddb0a81dd8aff4841
Amr Abdelraouf <ga47wuj@mytum.de>
2017-08-13 18:42:55 +0200

    add wound-edit component

---
+++ src/app/wound-edit/wound-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wound-edit',
+  templateUrl: './wound-edit.component.html',
+  styles: []
+})
+export class WoundEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
